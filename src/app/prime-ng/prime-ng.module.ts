33453839862a310ad0f1a6017fc5f9545952c056
sebastian461 <sebastiano-461@hotmail.com>
2024-03-25 17:23:41 -0500

    Experiencia

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 
 import { PanelModule } from 'primeng/panel';
 import { FieldsetModule } from 'primeng/fieldset';
+import { DividerModule } from 'primeng/divider';
 
 @NgModule({
-  exports: [PanelModule, FieldsetModule],
+  exports: [PanelModule, FieldsetModule, DividerModule],
 })
 export class PrimeNgModule {}
