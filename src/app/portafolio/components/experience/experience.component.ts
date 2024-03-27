30291b4f25cf72d6abafcb0ad7150eef65e1aeaf
sebastian461 <sebastiano-461@hotmail.com>
2024-03-26 22:35:58 -0500

    Experience component

---
+++ src/app/portafolio/components/experience/experience.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'portafolio-experience',
+  templateUrl: './experience.component.html',
+  styles: ``,
+})
+export class ExperienceComponent {}
--- src/app/portafolio/portafolio.module.ts
+++ src/app/portafolio/portafolio.module.ts
@@ -5,9 +5,10 @@ import { PortafolioRoutingModule } from './portafolio-routing.module';
 import { PaginaPrincipalComponent } from './pages/pagina-principal/pagina-principal.component';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 import { AboutComponent } from './components/about/about.component';
+import { ExperienceComponent } from './components/experience/experience.component';
 
 @NgModule({
-  declarations: [PaginaPrincipalComponent, AboutComponent],
+  declarations: [PaginaPrincipalComponent, AboutComponent, ExperienceComponent],
   imports: [CommonModule, PortafolioRoutingModule, PrimeNgModule],
 })
 export class PortafolioModule {}
