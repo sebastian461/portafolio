c0a765c2a248fc0737ae79c22d3714c181da9f9e
sebastian461 <sebastiano-461@hotmail.com>
2024-03-26 22:39:05 -0500

    Education component

---
+++ src/app/portafolio/components/education/education.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'portafolio-education',
+  templateUrl: './education.component.html',
+  styles: ``,
+})
+export class EducationComponent {}
